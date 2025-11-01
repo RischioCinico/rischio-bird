e15dcfa8e53bb9406fc28beffb882d4f17482a8f
RischioCinico <100477808+RischioCinico@users.noreply.github.com>
2025-11-01 22:10:36 +0100

    Update rischio-bird.js

--- rischio-bird.js
+++ rischio-bird.js
@@ -55,187 +55,110 @@ lockPref("privacy.trackingprotection.lower_network_priority", true);
    FINGERPRINTING
    ----------------------------------------------------------------------------------- */
 
+// Non usare colori di sistema
+lockPref("widget.non-native-theme.use-theme-accent", false);
+lockPref("browser.display.use_system_colors", false);
+lockPref("ui.use_standins_for_native_colors", true);
 
+// Arrotonda le dimensioni della finestra per prevenire il tracciamento
+lockPref("privacy.window.maxInnerHeight", 900);
+lockPref("privacy.window.maxInnerWidth", 1600);
 
+// Disabilita i selettori CSS per i link visitati per prevenire il "history sniffing"
+lockPref("layout.css.visited_links_enabled", false);
 
-/*** 001 DATA COLLECTION ***/
+// Disabilita l'API della batteria per prevenire il fingerprinting
+lockPref("dom.battery.enabled", false);
 
-// Disable Crash Reporting
-lockPref("breakpad.reportURL", "");
-lockPref("browser.crashReports.crashPull", false);
-lockPref("browser.crashReports.requestedNeverShowAgain", true);
-lockPref("browser.crashReports.unsubmittedCheck.autoSubmit2", false);
-lockPref("browser.crashReports.unsubmittedCheck.enabled", false);
-lockPref("toolkit.crashreporter.include_context_heap", false);
-
-// Disable Data Reporting & Telemetry
-lockPref("browser.safebrowsing.features.emailtracking.datacollection.update", false);
-lockPref("captchadetection.actor.enabled", false);
-lockPref("captchadetection.hasUnsubmittedData", false);
-lockPref("captchadetection.loglevel", "Off");
-lockPref("datareporting.dau.cachedUsageProfileID", "beefbeef-beef-beef-beef-beeefbeefbee");
-lockPref("datareporting.dau.cachedUsageProfileGroupID", "b0bacafe-b0ba-cafe-b0ba-cafeb0bacafe");
-lockPref("datareporting.policy.dataSubmissionPolicyBypassNotification", true);
-lockPref("datareporting.policy.firstRunURL", "");
-lockPref("datareporting.usage.uploadEnabled", false);
-lockPref("network.traffic_analyzer.enabled", false);
-lockPref("network.trr.confirmation_telemetry_enabled", false);
-lockPref("privacy.imageInputTelemetry.enableTestMode", false);
-lockPref("privacy.trackingprotection.emailtracking.data_collection.enabled", false);
-lockPref("telemetry.fog.test.activity_limit", -1);
-lockPref("telemetry.fog.test.inactivity_limit", -1);
-lockPref("telemetry.fog.init_on_shutdown", false);
-lockPref("telemetry.fog.test.localhost_port", 70000);
-lockPref("telemetry.number_of_site_origin.min_interval", 999999999);
-lockPref("toolkit.content-background-hang-monitor.disabled", true);
-lockPref("toolkit.telemetry.bhrPing.enabled", false);
-lockPref("toolkit.telemetry.cachedClientID", "c0ffeec0-ffee-c0ff-eec0-ffeec0ffeec0"); // [HIDDEN]
-lockPref("toolkit.telemetry.cachedProfileGroupID", "decafdec-afde-cafd-ecaf-decafdecafde"); // [HIDDEN]
-lockPref("toolkit.telemetry.collectInterval", 999999999); // [HIDDEN]
-lockPref("toolkit.telemetry.dap.helper.hpke", "");
-lockPref("toolkit.telemetry.dap.helper.url", "");
-lockPref("toolkit.telemetry.dap.leader.hpke", "");
-lockPref("toolkit.telemetry.dap.leader.url", "");
-lockPref("toolkit.telemetry.dap.logLevel", "Off");
-lockPref("toolkit.telemetry.eventping.maximumFrequency", 999999999); // [HIDDEN]
-lockPref("toolkit.telemetry.eventping.minimumFrequency", 999999999); // [HIDDEN]
-lockPref("toolkit.telemetry.firstShutdownPing.enabled", false); // [HIDDEN]
-lockPref("toolkit.telemetry.healthping.enabled", false); // [HIDDEN]
-lockPref("toolkit.telemetry.initDelay", 999999999); // [HIDDEN]
-lockPref("toolkit.telemetry.log.level", "Fatal"); // [HIDDEN]
-lockPref("toolkit.telemetry.minSubsessionLength", 999999999); // [HIDDEN]
-lockPref("toolkit.telemetry.newProfilePing.delay", 999999999); // [HIDDEN]
-lockPref("toolkit.telemetry.newProfilePing.enabled", false); // [HIDDEN]
-lockPref("toolkit.telemetry.previousBuildID", ""); // [HIDDEN]
-lockPref("toolkit.telemetry.reportingpolicy.firstRun", false); // [HIDDEN]
-lockPref("toolkit.telemetry.scheduler.idleTickInterval", 999999999); // [HIDDEN]
-lockPref("toolkit.telemetry.scheduler.tickInterval", 999999999); // [HIDDEN]
-lockPref("toolkit.telemetry.server", "data;");
-lockPref("toolkit.telemetry.server_owner", "");
-lockPref("toolkit.telemetry.shutdownPingSender.backgroundtask.enabled", false);
-lockPref("toolkit.telemetry.shutdownPingSender.enabled", false);
-lockPref("toolkit.telemetry.testing.disableFuzzingDelay", false);
-lockPref("toolkit.telemetry.testing.suppressPingsender", true); // [HIDDEN]
-lockPref("toolkit.telemetry.translations.logLevel", "Off");
-lockPref("toolkit.telemetry.unified", false);
-lockPref("toolkit.telemetry.untrustedModulesPing.frequency", 999999999); // [HIDDEN]
-lockPref("toolkit.telemetry.updatePing.enabled", false);
-lockPref("toolkit.telemetry.user_characteristics_ping.logLevel", "Off");
-lockPref("toolkit.telemetry.user_characteristics_ping.opt-out", true);
-lockPref("urlclassifier.features.emailtracking.datacollection.allowlistTables", "");
-lockPref("urlclassifier.features.emailtracking.datacollection.blocklistTables", "");
-
-// Disable Origin Trials
-lockPref("dom.origin-trials.enabled", false);
-
-// Remove partner attribution
+// Rimuove stringhe di identificazione
 lockPref("app.distributor", "");
 lockPref("app.distributor.channel", "");
 lockPref("mozilla.partner.id", "");
 
+/* -----------------------------------------------------------------------------------
+   PROXY
+   ----------------------------------------------------------------------------------- */
 
-/*** 002 MOZILLA CRAP™ ***/
+// Impedisci il failover automatico a connessioni dirette (non-proxy)
+lockPref("network.proxy.failover_direct", false);
 
-// Clear unnecessary/undesired Mozilla URLs
-lockPref("app.feedback.baseURL", "");
-lockPref("datareporting.healthreport.infoURL", "");
-lockPref("extensions.recommendations.privacyPolicyUrl", "");
-lockPref("toolkit.datacollection.infoURL", "");
+// Usa il proxy per la risoluzione DNS remota (previene i DNS leaks)
+lockPref("network.proxy.socks_remote_dns", true);
 
-// Disable add-on/feature recommendations
-lockPref("browser.discovery.enabled", false);
-lockPref("browser.translations.panelShown", true); // [HIDDEN]
-lockPref("extensions.getAddons.browseAddons", ""); // [HIDDEN]
-lockPref("extensions.getAddons.discovery.api_url", "data;");
-lockPref("extensions.getAddons.showPane", false); // [HIDDEN]
-lockPref("extensions.htmlaboutaddons.recommendations.enabled", false);
-lockPref("extensions.recommendations.hideNotice", true); // [HIDDEN]
-lockPref("extensions.recommendations.themeRecommendationUrl", "");
-lockPref("extensions.ui.lastCategory", "addons://list/extension"); // [HIDDEN]
-lockPref("extensions.webservice.discoverURL", ""); // [HIDDEN - non-Thunderbird]
+// Previene i WebRTC IP leaks forzando l'uso del proxy
+lockPref("media.peerconnection.ice.proxy_only_if_behind_proxy", true);
 
-// Disable Fakespot
-lockPref("toolkit.shopping.ohttpConfigURL", "");
-lockPref("toolkit.shopping.ohttpRelayURL", "");
+/* -----------------------------------------------------------------------------------
+   CONNESSIONI SICURE
+   ----------------------------------------------------------------------------------- */
 
-// Disable fetching Firefox Relay's "allowlist" (list of sites known to support Relay)
-lockPref("signon.firefoxRelay.allowListRemoteSettingsCollection", ""); // [HIDDEN]
+// Blocca le negoziazioni di connessione insicure per prevenire gli attacchi di "downgrade"
+lockPref("security.ssl.treat_unsafe_negotiation_as_broken", true);
 
-// Disable fetching Password Manager rules remotely by default
-lockPref("signon.recipes.remoteRecipes.enabled", false);
+// Avvisa sempre quando si invia un modulo da HTTP a HTTPS, anche su IP locali
+lockPref("security.insecure_field_warning.ignore_local_ip_address", false);
 
-// Disable Firefox Messaging System targeting information background updates
-lockPref("app.update.background.messaging.targeting.snapshot.intervalSec", -1);
+// Disabilita l'invio di richieste HTTP in background nella modalità Solo HTTPS
+lockPref("dom.security.https_only_mode_send_http_background_request", false);
 
-// Disable Firefox Relay by default
-lockPref("signon.firefoxRelay.feature", "disabled");
+// Disabilita i certificati root di terze parti a livello di sistema operativo per prevenire gli attacchi MITM
+lockPref("security.certerrors.mitm.auto_enable_enterprise_roots", false);
+lockPref("security.enterprise_roots.enabled", false);
 
-// Disable "Privacy-Preserving Attribution"
-lockPref("dom.origin-trials.private-attribution.state", 2);
+// Assicura che HTTP/3 non venga disabilitato in presenza di certificati root di terze parti
+lockPref("network.http.http3.disable_when_third_party_roots_found", false);
 
-// Disable Mozilla nags/promotions
-lockPref("browser.promo.cookiebanners.enabled", false);
-lockPref("browser.promo.focus.enabled", false);
-lockPref("browser.promo.pin.enabled", false);
-lockPref("browser.send_to_device_locales", "");
-lockPref("browser.vpn_promo.enabled", false);
-lockPref("privacy.trackingprotection.allow_list.hasUserInteractedWithETPSettings", true);
+// Disabilita TLS 1.3 0-RTT per prevenire attacchi
+lockPref("security.tls.enable_0rtt_data", false);
 
-// Disable Remote Permissions
-lockPref("permissions.manager.remote.enabled", false);
+// Abilita i controlli di revoca CRLite e dà loro la priorità su OCSP
+lockPref("security.OCSP.enabled", 0);
+lockPref("security.ssl.enable_ocsp_stapling", false);
+lockPref("security.pki.crlite_mode", 2);
+lockPref("security.csp.reporting.enabled", false);
+lockPref("security.remote_settings.crlite_filters.enabled", true);
 
-// Opt out of add-on metadata updates
-lockPref("extensions.getAddons.cache.enabled", false);
+// Fornisce una protezione extra bloccando completamente le connessioni in caso di mancata corrispondenza del certificato
+lockPref("security.cert_pinning.enforcement_level", 2);
 
-// Remove special privileges from Mozilla domains
-lockPref("browser.tabs.remote.separatePrivilegedMozillaWebContentProcess", false);
-lockPref("browser.tabs.remote.separatedMozillaDomains", "");
-lockPref("dom.ipc.processCount.privilegedmozilla", 0);
-lockPref("extensions.webextensions.restrictedDomains", "");
-lockPref("permissions.manager.defaultsUrl", "");
-lockPref("svg.context-properties.content.allowed-domains", "");
+// Consenti solo override temporanei per gli errori del certificato (per sessione)
+lockPref("security.certerrors.permanentOverride", false);
 
-// Skip Mozilla's `Privacy Notice` and `Terms of Use`
-lockPref("datareporting.policy.dataSubmissionPolicyAcceptedVersion", 999);
-lockPref("datareporting.policy.dataSubmissionPolicyNotifiedTime", "32503679999000");
-lockPref("termsofuse.acceptedDate", "32503679999000");
-lockPref("termsofuse.acceptedVersion", 999);
-lockPref("termsofuse.bypassNotification", true);
+// Mostra informazioni tecniche dettagliate sulle pagine di errore
+lockPref("browser.xul.error_pages.expert_bad_cert", true);
 
-// Enable TCP/dFPI
-lockPref("network.cookie.cookieBehavior", 5);
-lockPref("network.cookie.cookieBehavior.optInPartitioning", true);
+// Disabilita TLS sessione identifiers
+lockPref("security.ssl.disable_session_identifiers", true);
 
-// Ignore less restricted referer policies (than the default)
-lockPref("network.http.referer.disallowCrossSiteRelaxingDefault.top_navigation", true);
+/* -----------------------------------------------------------------------------------
+   CONNESSIONI IMPLICITE
+   ----------------------------------------------------------------------------------- */
 
+// Disabilita il monitoraggio dello stato della connessione di rete
+lockPref("network.manage-offline-status", false);
 
-/*** 004 FINGERPRINTING PROTECTION ***/
+// Disabilita l'API Beacon (Navigator.sendBeacon)
+lockPref("beacon.enabled", false);
 
-// Disable VP9 Benchmark
-lockPref("media.benchmark.vp9.threshold", 0);
+// Disable Early Hints (Like Cromite)
+lockPref("network.early-hints.enabled", false);
+lockPref("network.early-hints.over-http-v1-1.enabled", false);
+lockPref("network.early-hints.preconnect.enabled", false);
+lockPref("network.early-hints.preconnect.max_connections", 0);
 
-// Do not use the theme's toolbar color scheme for in-content pages by default
-lockPref("browser.theme.unified-color-scheme", false);
+// Disable Network Prefetching
+lockPref("dom.prefetch_dns_for_anchor_http_document", false);
+lockPref("network.dns.disablePrefetch", true);
+lockPref("network.dns.disablePrefetchFromHTTPS", true);
+lockPref("network.prefetch-next", false);
 
-// Prevent using system accent colors
-lockPref("widget.non-native-theme.use-theme-accent", false);
+// Disable Preconnect
+lockPref("network.preconnect", false);
 
-// Prevent using system colors
-lockPref("browser.display.use_system_colors", false);
-lockPref("ui.use_standins_for_native_colors", true);
+// Prevent middle mouse clicks from pasting clipboard contents by default
+lockPref("middlemouse.paste", false);
+lockPref("middlemouse.openNewWindow", false);
 
-// Prompt to spoof locale to en-US
-lockPref("privacy.spoof_english", 0);
-
-// Reset the fingerprinting randomization key daily (in addition to per-session/when the browser restarts)
-lockPref("privacy.resistFingerprinting.randomization.daily_reset.enabled", true);
-lockPref("privacy.resistFingerprinting.randomization.daily_reset.private.enabled", true);
-
-// Round window sizes
-lockPref("browser.startup.blankWindow", false);
-lockPref("privacy.window.maxInnerHeight", 900);
-lockPref("privacy.window.maxInnerWidth", 1600);
 
 
 // Fine
