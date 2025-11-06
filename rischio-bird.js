29d529f1dc8927fcbd56b300a87e88847631b0de
RischioCinico <100477808+RischioCinico@users.noreply.github.com>
2025-11-06 07:00:44 +0100

    Update rischio-bird.js

--- rischio-bird.js
+++ rischio-bird.js
@@ -1,1 +1,271 @@
 
+/* -----------------------------------------------------------------------------------
+   001 DATA COLLECTION
+   ----------------------------------------------------------------------------------- */
+
+lockPref("toolkit.aboutLogging.uploadProfileToCloud", false);
+lockPref("toolkit.aboutlogging.uploadProfileUrl", "");
+
+lockPref("toolkit.coverage.enabled", false);
+lockPref("toolkit.coverage.endpoint.base", "");
+lockPref("toolkit.coverage.log-level", 70);
+lockPref("toolkit.coverage.opt-out", true);
+lockPref("toolkit.telemetry.coverage.opt-out", true);
+
+lockPref("breakpad.reportURL", "");
+lockPref("browser.crashReports.crashPull", false);
+lockPref("browser.crashReports.requestedNeverShowAgain", true);
+lockPref("browser.crashReports.unsubmittedCheck.autoSubmit2", false);
+lockPref("browser.crashReports.unsubmittedCheck.enabled", false);
+lockPref("toolkit.crashreporter.include_context_heap", false);
+
+lockPref("browser.safebrowsing.features.emailtracking.datacollection.update", false);
+lockPref("captchadetection.actor.enabled", false);
+lockPref("captchadetection.hasUnsubmittedData", false);
+lockPref("captchadetection.loglevel", "Off");
+lockPref("datareporting.dau.cachedUsageProfileID", "beefbeef-beef-beef-beef-beeefbeefbee");
+lockPref("datareporting.dau.cachedUsageProfileGroupID", "b0bacafe-b0ba-cafe-b0ba-cafeb0bacafe");
+lockPref("datareporting.healthreport.uploadEnabled", false);
+lockPref("datareporting.policy.dataSubmissionEnabled", false);
+lockPref("datareporting.policy.dataSubmissionPolicyBypassNotification", true);
+lockPref("datareporting.policy.firstRunURL", "");
+lockPref("datareporting.usage.uploadEnabled", false);
+lockPref("dom.security.unexpected_system_load_telemetry_enabled", false);
+lockPref("extensions.dataCollectionPermissions.enabled", false);
+lockPref("extensions.telemetry.EnvironmentAddonBuilder", false);
+lockPref("network.jar.record_failure_reason", false);
+lockPref("network.traffic_analyzer.enabled", false);
+lockPref("network.trr.confirmation_telemetry_enabled", false);
+lockPref("nimbus.telemetry.targetingContextEnabled", false);
+lockPref("privacy.imageInputTelemetry.enableTestMode", false);
+lockPref("privacy.trackingprotection.emailtracking.data_collection.enabled", false);
+lockPref("telemetry.fog.aboutGlean.debugTag", "");
+lockPref("telemetry.fog.artifact_build", false);
+lockPref("telemetry.fog.test.activity_limit", -1);
+lockPref("telemetry.fog.test.inactivity_limit", -1);
+lockPref("telemetry.fog.init_on_shutdown", false);
+lockPref("telemetry.fog.test.localhost_port", 70000);
+lockPref("telemetry.glean.internal.finalInactive", false);
+lockPref("telemetry.glean.internal.maxPingsPerMinute", 0);
+lockPref("telemetry.number_of_site_origin.min_interval", 999999999);
+lockPref("toolkit.content-background-hang-monitor.disabled", true);
+lockPref("toolkit.telemetry.archive.enabled", false);
+lockPref("toolkit.telemetry.bhrPing.enabled", false);
+lockPref("toolkit.telemetry.cachedClientID", "c0ffeec0-ffee-c0ff-eec0-ffeec0ffeec0");
+lockPref("toolkit.telemetry.cachedProfileGroupID", "decafdec-afde-cafd-ecaf-decafdecafde");
+lockPref("toolkit.telemetry.collectInterval", 999999999);
+lockPref("toolkit.telemetry.dap.helper.hpke", "");
+lockPref("toolkit.telemetry.dap.helper.url", "");
+lockPref("toolkit.telemetry.dap.leader.hpke", "");
+lockPref("toolkit.telemetry.dap.leader.url", "");
+lockPref("toolkit.telemetry.dap.logLevel", "Off");
+lockPref("toolkit.telemetry.dap_enabled", false);
+lockPref("toolkit.telemetry.dap_task1_enabled", false);
+lockPref("toolkit.telemetry.dap_task1_taskid", "");
+lockPref("toolkit.telemetry.dap_visit_counting_enabled", false);
+lockPref("toolkit.telemetry.dap_visit_counting_experiment_list", "[]");
+lockPref("toolkit.telemetry.debugSlowSql", false);
+lockPref("toolkit.telemetry.enabled", false);
+lockPref("toolkit.telemetry.eventping.maximumFrequency", 999999999);
+lockPref("toolkit.telemetry.eventping.minimumFrequency", 999999999);
+lockPref("toolkit.telemetry.firstShutdownPing.enabled", false);
+lockPref("toolkit.telemetry.healthping.enabled", false);
+lockPref("toolkit.telemetry.initDelay", 999999999);
+lockPref("toolkit.telemetry.log.dump", false);
+lockPref("toolkit.telemetry.log.level", "Fatal");
+lockPref("toolkit.telemetry.minSubsessionLength", 999999999);
+lockPref("toolkit.telemetry.newProfilePing.delay", 999999999);
+lockPref("toolkit.telemetry.newProfilePing.enabled", false);
+lockPref("toolkit.telemetry.overrideUpdateChannel", "release");
+lockPref("toolkit.telemetry.previousBuildID", "");
+lockPref("toolkit.telemetry.reportingpolicy.firstRun", false);
+lockPref("toolkit.telemetry.scheduler.idleTickInterval", 999999999);
+lockPref("toolkit.telemetry.scheduler.tickInterval", 999999999);
+lockPref("toolkit.telemetry.send.overrideOfficialCheck", false);
+lockPref("toolkit.telemetry.server", "data;");
+lockPref("toolkit.telemetry.server_owner", "");
+lockPref("toolkit.telemetry.shutdownPingSender.backgroundtask.enabled", false);
+lockPref("toolkit.telemetry.shutdownPingSender.enabled", false);
+lockPref("toolkit.telemetry.shutdownPingSender.enabledFirstSession", false);
+lockPref("toolkit.telemetry.testing.disableFuzzingDelay", false);
+lockPref("toolkit.telemetry.testing.overridePreRelease", false);
+lockPref("toolkit.telemetry.testing.overrideProductsCheck", false);
+lockPref("toolkit.telemetry.testing.suppressPingsender", true);
+lockPref("toolkit.telemetry.translations.logLevel", "Off");
+lockPref("toolkit.telemetry.unified", false);
+lockPref("toolkit.telemetry.untrustedModulesPing.frequency", 999999999);
+lockPref("toolkit.telemetry.updatePing.enabled", false);
+lockPref("toolkit.telemetry.user_characteristics_ping.current_version", 0);
+lockPref("toolkit.telemetry.user_characteristics_ping.last_version_sent", 0);
+lockPref("toolkit.telemetry.user_characteristics_ping.logLevel", "Off");
+lockPref("toolkit.telemetry.user_characteristics_ping.opt-out", true);
+lockPref("toolkit.telemetry.user_characteristics_ping.send-once", false);
+lockPref("toolkit.telemetry.user_characteristics_ping.uuid", "");
+lockPref("urlclassifier.features.emailtracking.datacollection.allowlistTables", "");
+lockPref("urlclassifier.features.emailtracking.datacollection.blocklistTables", "");
+
+lockPref("app.normandy.run_interval_seconds", 0);
+lockPref("app.shield.optoutstudies.enabled", false);
+lockPref("messaging-system.rsexperimentloader.collection_id", "");
+lockPref("nimbus.appId", "");
+lockPref("nimbus.profileId", "");
+lockPref("nimbus.profilesdatastoreservice.enabled", false);
+lockPref("nimbus.profilesdatastoreservice.read.enabled", false);
+lockPref("nimbus.profilesdatastoreservice.sync.enabled", false);
+
+lockPref("about.glean.redesign.enabled", false);
+
+lockPref("dom.origin-trials.enabled", false);
+
+lockPref("app.distributor", "");
+lockPref("app.distributor.channel", "");
+lockPref("mozilla.partner.id", "");
+
+/* -----------------------------------------------------------------------------------
+   002 MOZILLA CRAP
+   ----------------------------------------------------------------------------------- */
+
+// Clear unnecessary/undesired Mozilla URLs
+lockPref("app.feedback.baseURL", "");
+lockPref("datareporting.healthreport.infoURL", "");
+lockPref("extensions.recommendations.privacyPolicyUrl", "");
+lockPref("toolkit.datacollection.infoURL", "");
+
+// Disable add-on/feature recommendations
+lockPref("browser.discovery.enabled", false);
+lockPref("browser.translations.mostRecentTargetLanguages", "en-US");
+lockPref("browser.translations.panelShown", true);
+lockPref("extensions.getAddons.browseAddons", "");
+lockPref("extensions.getAddons.discovery.api_url", "data;");
+lockPref("extensions.getAddons.showPane", false);
+lockPref("extensions.htmlaboutaddons.recommendations.enabled", false);
+lockPref("extensions.recommendations.hideNotice", true);
+lockPref("extensions.recommendations.themeRecommendationUrl", "");
+lockPref("extensions.ui.lastCategory", "addons://list/extension");
+lockPref("extensions.webservice.discoverURL", "");
+
+// Disable DoH Rollout/heuristics/steering
+lockPref("doh-rollout._testing", true);
+lockPref("doh-rollout.disable-heuristics", true);
+lockPref("doh-rollout.doneFirstRun", true);
+lockPref("doh-rollout.doorhanger-decision", "UIDisabled");
+lockPref("doh-rollout.enabled", false);
+lockPref("doh-rollout.mode", 5);
+lockPref("doh-rollout.provider-steering.enabled", false);
+lockPref("doh-rollout.provider-steering.provider-list", "");
+lockPref("doh-rollout.self-enabled", false);
+lockPref("doh-rollout.skipHeuristicsCheck", true);
+lockPref("doh-rollout.trr-selection.enabled", false);
+lockPref("doh-rollout.trr-selection.provider-list", "");
+lockPref("doh-rollout.uri", "");
+lockPref("network.android_doh.autoselect_enabled", false);
+
+// Disable DoH performance measurements
+lockPref("doh-rollout.trrRace.canonicalDomain", "");
+lockPref("doh-rollout.trrRace.complete", true);
+lockPref("doh-rollout.trrRace.enabled", false);
+lockPref("doh-rollout.trrRace.popularDomains", "");
+lockPref("doh-rollout.trrRace.randomSubdomainCount", 0);
+
+// Disable 'Essential Domains Fallback'
+lockPref("network.essential_domains_fallback", false);
+
+// Disable Fakespot
+lockPref("toolkit.shopping.ohttpConfigURL", "");
+lockPref("toolkit.shopping.ohttpRelayURL", "");
+
+// Disable fetching Firefox Relay's "allowlist" (list of sites known to support Relay)
+lockPref("signon.firefoxRelay.allowListRemoteSettingsCollection", "");
+
+// Disable fetching Password Manager rules remotely by default
+lockPref("signon.recipes.remoteRecipes.enabled", false);
+
+// Disable Firefox Messaging System targeting information background updates
+lockPref("app.update.background.messaging.targeting.snapshot.intervalSec", -1); 
+
+// Disable Firefox Relay by default
+lockPref("signon.firefoxRelay.feature", "disabled");
+
+// Disable "Interest-based Content Relevance Ranking and Personalization"
+lockPref("toolkit.contentRelevancy.enabled", false);
+lockPref("toolkit.contentRelevancy.ingestEnabled", false);
+lockPref("toolkit.contentRelevancy.log", false);
+
+// Disable Mozilla nags/promotions
+lockPref("browser.promo.cookiebanners.enabled", false);
+lockPref("browser.promo.focus.enabled", false);
+lockPref("browser.promo.pin.enabled", false);
+lockPref("browser.send_to_device_locales", "");
+lockPref("browser.vpn_promo.enabled", false);
+lockPref("privacy.trackingprotection.allow_list.hasUserInteractedWithETPSettings", true);
+
+// Disable "Privacy-Preserving Attribution"
+lockPref("dom.origin-trials.private-attribution.state", 2);
+lockPref("dom.private-attribution.submission.enabled", false);
+
+// Disable Remote Permissions
+lockPref("permissions.manager.remote.enabled", false);
+
+// Disable Remote Settings 'Preview' Buckets
+lockPref("services.settings.preview_enabled", false);  
+
+// Disable the Web Compatibility Reporter
+lockPref("extensions.webcompat-reporter.enabled", false);
+
+// Opt out of add-on metadata updates
+lockPref("extensions.getAddons.cache.enabled", false);
+
+// Remove special privileges from Mozilla domains
+lockPref("browser.tabs.remote.separatePrivilegedMozillaWebContentProcess", false);
+lockPref("browser.tabs.remote.separatedMozillaDomains", "");
+lockPref("dom.ipc.processCount.privilegedmozilla", 0);
+lockPref("extensions.webextensions.restrictedDomains", "");
+lockPref("permissions.manager.defaultsUrl", "");
+lockPref("svg.context-properties.content.allowed-domains", "");
+
+// Remove tracking parameters from Mozilla URLs + prevent exposing locale & unnecessary information
+lockPref("browser.backup.template.fallback-download.aurora", "https://www.mozilla.org/firefox/channel/desktop/#developer");
+lockPref("browser.backup.template.fallback-download.beta", "https://www.mozilla.org/firefox/channel/desktop/#beta");
+lockPref("browser.backup.template.fallback-download.esr", "https://www.mozilla.org/firefox/enterprise/#download");
+lockPref("browser.backup.template.fallback-download.nightly", "https://www.mozilla.org/firefox/channel/desktop/#nightly");
+lockPref("browser.backup.template.fallback-download.release", "https://www.mozilla.org/firefox/download/thanks/?s=direct");
+lockPref("extensions.abuseReport.amoFormURL", "https://addons.mozilla.org/feedback/addon/%addonID%/");
+lockPref("extensions.blocklist.addonItemURL", "https://addons.mozilla.org/blocked-addon/%addonID%/%addonVersion%/");
+lockPref("signon.firefoxRelay.learn_more_url", "https://support.mozilla.org/kb/relay-integration#w_frequently-asked-questions");
+lockPref("signon.firefoxRelay.manage_url", "https://relay.firefox.com/accounts/profile/");
+lockPref("signon.firefoxRelay.privacy_policy_url", "https://www.mozilla.org/privacy/subscription-services/");
+lockPref("signon.firefoxRelay.terms_of_service_url", "https://www.mozilla.org/about/legal/terms/subscription-services/");
+
+// Skip Mozilla's `Privacy Notice` and `Terms of Use`
+lockPref("datareporting.policy.dataSubmissionPolicyAcceptedVersion", 999);
+lockPref("datareporting.policy.dataSubmissionPolicyNotifiedTime", "32503679999000");
+lockPref("termsofuse.acceptedDate", "32503679999000");
+lockPref("termsofuse.acceptedVersion", 999);
+lockPref("termsofuse.bypassNotification", true);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Controllo versione
+lockPref("rischio.bird", "OK");
